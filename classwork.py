62e703418e45b0eb365c90ff1a1188e73a4ed366
masha27092009 <161881026+masha27092009@users.noreply.github.com>
2024-03-06 09:29:46 +0000

    add to main.py

---
+++ classwork.py
@@ -0,0 +1,13 @@
+def min_n_valiables (elements):
+    min_v = elements (0)
+    for x in elements:
+        if x < min_v:
+            min_v = x
+            return min_v
+
+# elements = [6**6, 7**5, 5**7]
+elements = [5,1,9,0,7]
+assert min_n_valiables(elements) == 0
+print('min value of elements=', min_n_valiables) 
+
+assert min_n_valiables(elements) == 0
